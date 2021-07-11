357eb6ce4c261800f03dd81546433c0188a8b8d1
alexisvt <alexisvt@gmail.com>
2021-07-11 09:02:27 -0600

    Create controllers and routes

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 import express from 'express';
+import helmet from 'helmet';
+import morgan from 'morgan';
 
 import postRoutes from './routes/post';
 
 const app = express();
+app.use(helmet())
+app.use(morgan("dev"));
 
 app.use('/', postRoutes);
 
--- routes/post.js
+++ routes/post.js
@@ -1,9 +1,9 @@
 import express from 'express';
 
+import { getPosts } from '../controllers/post';
+
 const route = express.Router();
 
-route.get('/', (req, res) => {
-  res.send("Hello world!");
-})
+route.get('/', getPosts)
 
 export default route;
